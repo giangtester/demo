2ce078ce84c707c4417b9d05d651ed5018047011
giang.tester <giang.tester@gmail.com>
2025-02-22 14:18:26 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        int myNum = 5;               // integer (whole number)
+        float myFloatNum = 5.66f;    // floating point number
+        char myLetter = 'D';         // character
+        boolean myBool = true;       // boolean
+        String myText = "Hello";     // String
+        System.out.println(myNum); /* conment */
+        System.out.println(myFloatNum);
+        System.out.println(myLetter);
+        System.out.println(myBool);
+        System.out.println(myText);
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 3;
+
+        String s = (a % b == 0) ? "a chia het cho b" : "a khong chia het cho b";
+        System.out.println(s);
+    }
+}
