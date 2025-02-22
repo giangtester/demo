796cee71ac4d35b817036e5283394ea5271bc469
giang.tester <giang.tester@gmail.com>
2025-02-22 16:04:44 +0700

    update code

---
+++ src/hocluc.java
@@ -0,0 +1,26 @@
+public class hocluc {
+    public static void main(String[] args)
+    { int score  = 50;
+        if (score >=90) {
+            System.out.println( "xuất xắc");
+        }
+          else if (score >70) {
+              System.out.println("khá");
+        }
+          else if (score >=60) {
+              System.out.println("Trung bình khá");
+        }
+          else {
+              System.out.println("trung bình");       }
+
+    }
+}
+/* if-else
+if (điều_kiện) {
+    // Khối lệnh thực thi nếu điều kiện đúng
+} else if (điều_kiện_2) {
+    // Khối lệnh thực thi nếu điều kiện_2 đúng
+} else {
+    // Khối lệnh mặc định
+}
+ */
