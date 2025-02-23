28df795397aacdc90363a36c8294708926e457d0
giang.tester <giang.tester@gmail.com>
2025-02-23 14:08:21 +0700

    demo

--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,11 @@
 public class Test {
     public static void main(String[] args) {
-        int a = 20;
+        int a = 100;
         int b = 3;
 
         String s = (a % b == 0) ? "a chia het cho b" : "a khong chia het cho b";
+        String nuocsoi = (a == 100) ? "nước sôi" : (a>0) ? "nước ấm": "nước lạnh";
+        System.out.println(nuocsoi);
         System.out.println(s);
     }
 }
--- src/toantu.java
+++ src/toantu.java
@@ -1,7 +1,25 @@
 public class toantu
 { public static void main(String[] args){
     int a=10,b=5;
+    int min = (a<b) ? a:b;
+    int max = (a>b) ? a:b;
+    String ketqua= (a%2 ==0 )? "a la so chẵn" :"a là số lẻ" ;
+    String ketqua2 =(a>=0) ? "a là số dương": "a là số âm";
+    String ketqua3 = (a>0) ? "a là số dương" : (a<0) ? "a là số âm" : "a là số 0";
+    System.out.println(ketqua3);
+    System.out.println(ketqua2);
+    System.out.println(ketqua);
+    System.out.println("Số lơn hơn là: " + max);
+    System.out.println(min);
     System.out.println(a>b);
     System.out.println(a==b);
+    System.out.println((a>b)&&(b>0));
+    System.out.println((a<b)||(b<0));
+    System.out.println(!(a>b));
+    System.out.println(a++);
+    System.out.println(++a);
+    System.out.println(b--);
+    System.out.println(--b);
+
 }
 }
