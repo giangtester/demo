6e41fbe011057cdcfa448867d26c7d49927c33fc
giang.tester <giang.tester@gmail.com>
2025-02-24 22:13:41 +0700

    up

--- src/switch1/vd.java
+++ src/switch1/vd.java
@@ -1,4 +1,16 @@
 package switch1;
-
+import java.util.Scanner;
 public class vd {
+    public  static void main(String[] args){
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Nhập vào nhiệt độ");
+        double t=scanner.nextDouble();
+        if (t==100) {
+            System.out.println("nước đã đun sôi");
+        }
+        else {
+            System.out.println("nước chưa đun sôi");
+        }
+
+    }
 }
