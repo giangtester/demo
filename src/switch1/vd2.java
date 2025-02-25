b540ff668eaf26a74ef58c2974384533bb726f0f
giang.tester <giang.tester@gmail.com>
2025-02-25 21:43:20 +0700

    update

--- src/switch1/vd2.java
+++ src/switch1/vd2.java
@@ -1,4 +1,10 @@
 package switch1;
-
+enum ngay {th2,th3,th4,th5,th6, th7, cn}
 public class vd2 {
+    public static void main(String[] args){
+        ngay today =ngay.th3;
+        System.out.println("hôm nay là "+today);
+
+
+    }
 }
