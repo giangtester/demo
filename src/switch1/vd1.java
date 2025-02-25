5a2fa707807fa49a07f572802db706662c407556
giang.tester <giang.tester@gmail.com>
2025-02-25 21:56:03 +0700

    update

--- src/switch1/vd1.java
+++ src/switch1/vd1.java
@@ -1,4 +1,8 @@
 package switch1;
-
+enum lop {lop1,lop2,lop3}
 public class vd1 {
+    public static void main(String[] args){
+        lop hocsinh=lop.lop1;
+        System.out.println(hocsinh);
+    }
 }
