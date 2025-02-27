0f4c602891ebbb2d8fa3a7dc8db5b4e917cfbbd9
giang.tester <giang.tester@gmail.com>
2025-02-27 21:14:33 +0700

    test

--- src/switch1/vd3.java
+++ src/switch1/vd3.java
@@ -1,4 +1,11 @@
 package switch1;
 
 public class vd3 {
+    public void sayhello(){
+        int n=10;
+        System.out.println("gía trị của n là "+ n); }
+    public static void main (String[] args){
+       vd3 bienlocal = new vd3();
+        bienlocal.sayhello();
+    }
 }
