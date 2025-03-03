d0cd9a1690c685c8615a12e5f5a51285be71cb54
giang.tester <giang.tester@gmail.com>
2025-03-03 19:13:27 +0700

    commit

--- src/nhap_xuat/vd2.java
+++ src/nhap_xuat/vd2.java
@@ -1,4 +1,11 @@
 package nhap_xuat;
-
+import java.util.Scanner;
 public class vd2 {
-}
+    public static void main(String[] args){
+        String ten;
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("vui long nhap ten: ");
+        ten= scanner.nextLine();
+        scanner.close();
+        System.out.print(ten);
+}}
