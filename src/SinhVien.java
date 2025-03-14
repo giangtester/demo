3b24efa7ab2ce31782df2fb8b4a8c0cff5b99ee9
giang.tester <giang.tester@gmail.com>
2025-03-14 22:24:13 +0700

    commit

---
+++ src/SinhVien.java
@@ -0,0 +1,16 @@
+// Đây là Class (Bản Thiết Kế)
+public class SinhVien {
+    // Các thuộc tính (đặc điểm)
+    String hoTen;
+    int tuoi;
+    String maSinhVien;
+
+    // Phương thức (hành động)
+    public void hocBai() {
+        System.out.println(hoTen + " đang học bài");
+    }
+
+    public void thamDuLop() {
+        System.out.println(hoTen + " đang tham dự lớp học");
+    }
+}
